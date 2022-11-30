56bf8a4da537679d4d81e658d6c21a5af5c3d02b
abrupttechnology <reesemooregillis@gmail.com>
2022-11-29 18:04:53 -0600

    Test

--- app/src/main/java/org/com/testing/with/simpletest/MainActivity.kt
+++ app/src/main/java/org/com/testing/with/simpletest/MainActivity.kt
@@ -1,11 +1,30 @@
 package org.com.testing.with.simpletest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    var viewModel = MainViewModel()
+
+    val adapter = RVCustomAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val recyclerview = findViewById<RecyclerView>(R.id.mRecyclerView)
+        recyclerview.adapter = adapter
+
+        recyclerview.hasFixedSize()
+        recyclerview.layoutManager = LinearLayoutManager(this)
+
+        viewModel.data.observe(this) {
+            adapter.setDataList(it as MutableList<Article>)
+        }
     }
 }
